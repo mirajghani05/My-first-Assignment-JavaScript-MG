3ccadb7aa01536f39a557f5ce7b865927e6c754d
mirajghani05 <mirajghani05@gmail.com>
2025-12-01 00:34:34 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,89 @@
+//chapter 01 Assignment 01
+alert("Hello world.. Welcome To Our Website")
+//chapter 01 Assignment 02
+alert("Error! Please enter a valid password")
+//chapter 01 Assignment 03
+alert("Welcome to JS land... \n Happy coding!")
+//chapter 01 Assignment 04
+alert("Welcome to JS Land...")
+alert("Happy Coding!")
+//chapter 01 Assignment 05
+console.log("Hello..! I can run JS through my web browser console")
+//I Think chapter 1 is completely complete 💯 //
+
+//chapter 2 Assignment 01
+var username="";
+//chapter 2 Q2
+var myName="Miraj Ghani";
+//chapter 2 Q 3
+var message="Hello World"
+alert(message);
+//chapter 2 Q4
+var studentBio_name="Miraj Ghani"
+alert(studentBio_name);
+var studentBio_year="I am 19 year old"
+alert(studentBio_year);
+var studentBio_qualified="Learning Web And Application development"
+//chapter 2 Q5
+alert(studentBio_qualified);
+var oneSentence=" Pizza \n Pizz \n Piz \n Pi \n P"
+alert(oneSentence)
+
+//chapter 2 Q6
+var email="My Email Address is mirajghani05@gmail.com"
+alert(email)
+
+//chapter 2 Q7
+var book=" A Smarter way to learn JavaScript"
+alert(" I am trying to learn JavaScript from the Book" +book)
+//chapter 2 Q8
+document.write('<p>Yah! I can make HTML content just using JavaScript </p>')
+
+//chapter 2 Q9
+
+var store="’______£§©______`"
+alert(store)
+
+// CHAPTER 02 Completed //
+
+//Chapter 3 Q1
+
+var age="19 Year old"
+alert(age)
+
+//chapter 3 Q2
+
+var tracking="You have Visited this sitr 1st Time Welcome 🤗 "
+alert(tracking)
+
+//chapter 3 Q3
+
+var birthYear="My Birth Year is 2007"
+alert(birthYear)
+document.write('<p>My Birth Year is 2007 \n data type of my ddeclared variable is number</p>')
+
+//chapter 3 Q4
+
+var visitorName="Miraj Wants"
+var quantityT_shirt=" 5"
+var product=" T shirt"
+alert(visitorName+quantityT_shirt +product)
+
+document.write('<p>Miraj Wants 5 T shirt</p>')
+
+//CHAPTER 03 COMPLETED //
+
+//Chapter 4 Q1
+
+var name="This is legal var"
+var applePhone_33="This is also"
+var son$="And this is also"
+
+//Chapter 4 Q2
+var illegal="Name Capital letter Start, Number start 123,-+ are illegal"
+alert(illegal)
+
+//chapter 4 Q3
+
+document.write('<h1>Rule for naming variables in JS</h1>')
+document.write('<p>Variable name can only contain, number, $ and _, for example: $name, _ name or name \n variable name are sensitive \n variable should not be JS keyboard</p>')
